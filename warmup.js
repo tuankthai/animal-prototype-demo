83459d85c3882ff838939177526602670b196ec0
tuankthai <ttlive4jc@gmail.com>
2023-06-08 19:41:47 -0700

    done

--- warmup.js
+++ warmup.js
@@ -1,13 +1,21 @@
-function Animal(name) {
-    this.name = name;
-  }
+
+function Animal(name, color) {
+  this.name = name;
+  this.color = color;
+}
 
 
 
-  // Method for Animal
-  Animal.prototype.sound = function() {
-    console.log(this.name + " makes a sound");
-  };
+// Method for Animal
+Animal.prototype.sound = function () {
+  console.log(this.name + " makes a sound");
+};
 
+Animal.prototype.showColor = function () {
+  console.log(this.name + ` has a ${this.color}`)
+}
 
-  const animal = new Animal("Generic Animal");
+const animal = new Animal("Oink", "red");
+console.log(animal)
+animal.sound()
+animal.showColor()
